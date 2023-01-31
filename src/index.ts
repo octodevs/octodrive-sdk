ef5f15b749cdfeca65bf55076b311f73c1a23289
Minhyeok Park <pmh_only@pmh.codes>
2023-02-01 01:06:31 +0900

    test: add APIWrapper class test suit

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export * from './Exceptions'
 export * from './APIWrapper/APIEndpoints'
 export * from './APIWrapper/APIFetcher'
 export * from './APIWrapper/APIWrapper'
+export * from './APIWrapper/APIWrapBuilder'
 
 export * from './Metadata/Metadata'
 export * from './EncryptedData/EncryptedData'
